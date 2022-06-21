63db2b6f49fc1dcc1baf086091cfd603f2effa52
Rahhal Rizqy <rarahhal@e1r5p1.1337.ma>
2022-06-21 10:39:31 +0100

    alpha_mirror

--- exam/exam_rank_2/alpha_mirror.c
+++ exam/exam_rank_2/alpha_mirror.c
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ulstr.c                                            :+:      :+:    :+:   */
+/*   alpha_mirror.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rarahhal <rarahhal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/21 10:24:59 by rarahhal          #+#    #+#             */
-/*   Updated: 2022/06/21 10:25:00 by rarahhal         ###   ########.fr       */
+/*   Created: 2022/06/21 10:24:51 by rarahhal          #+#    #+#             */
+/*   Updated: 2022/06/21 10:38:34 by rarahhal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
@@ -22,17 +22,17 @@ int main(int argc, char *argv[])
 	{
 		if (argv[1][i] >= 'a' && argv[1][i] <='z')
 		{
-			argv[1][i] -= 32;
+			argv[1][i] ;
 			write(1, &argv[1][i], 1);
 		}
 		else if (argv[1][i] >= 'A' && argv[1][i] <='Z')
 		{
-			argv[1][i] += 32;
+			argv[1][i] += ;
 			write(1, &argv[1][i], 1);
 		}
 		else
 			write(1, &argv[1][i], 1);
 	}
 	write(1, "\n", 1);
-	return (0);
+    return (0);
 }
